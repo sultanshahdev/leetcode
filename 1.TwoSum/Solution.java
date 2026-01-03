27efd3acd6719464d4dc35e71c3b1365c4889233
sultan <sultanshah.bscsf24@gmail.com>
2026-01-03 20:02:25 +0500

    Add Solution for LEETCODE QUESTION#1 TWO SUM

---
+++ 1.TwoSum/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> hm = new HashMap<>();
+
+        for(int i=0;i<arr.length;i++)
+        {
+            hm.put(arr[i],i);
+        }
+
+        for(int i=0;i<hm.size();i++)
+        {
+            int elementToBeFound = target-hm.get(i);
+            if(hm.contains(elementToBeFound) && hm.compu)
+        }
+        
+    }
+}
